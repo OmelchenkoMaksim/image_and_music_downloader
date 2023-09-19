8993bc24d222a4046042b5148ca96dc7c59b2d41
mvomelchenko <MVOMELCHEN@mts.ru>
2023-09-19 16:35:25 +0300

    remove Android View Model

--- app/src/main/java/com/example/storageformusicandimages/room/RoomViewModel.kt
+++ app/src/main/java/com/example/storageformusicandimages/room/RoomViewModel.kt
@@ -1,14 +1,13 @@
 package com.example.storageformusicandimages.room
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.storageformusicandimages.App
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class RoomViewModel(application: Application) : AndroidViewModel(application) {
+class RoomViewModel() : ViewModel() {
 
     private val db = App.database
     private val userDao = db.noteDao()
